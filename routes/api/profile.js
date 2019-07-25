667d0fbfd3698852972eda08935ed7b6ef8ed823
Jayden Shaw <jayden.shaw@gmail.com>
2019-07-25 10:36:21 -0600

    User profile create/update logic

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
@@ -22,5 +23,52 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post(
+    '/', 
+    [ 
+        auth, 
+            [ 
+                check('company', 'Company is required')
+                    .not()
+                    .isEmpty(),
+                check('email', 'Email is required')
+                    .not()
+                    .isEmpty()
+            ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const {
+            company, 
+            website,
+            location,
+            email
+        } = req.body;
+        const profileFields = {};
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (email) profileFields.email = email;
+
+        try {
+            let profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true, upsert: true }
+            );
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
