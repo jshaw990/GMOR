a8cc81ad199733341709fece3d24bf1714eaebe0
Jayden Shaw <jayden.shaw@gmail.com>
2019-07-25 09:16:12 -0600

    User Profile Schema created

---
+++ models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
